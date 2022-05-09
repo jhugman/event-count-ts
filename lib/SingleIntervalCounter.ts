ec520b4e8ae0f9576f3ad89ff74e31e6b92afdc2
James Hugman <james@hugman.tv>
2022-05-09 16:00:03 +0100

    Refactor to make querying easier

--- lib/SingleIntervalCounter.ts
+++ lib/SingleIntervalCounter.ts
@@ -7,48 +7,64 @@ export type Int = number
 export interface IntervalCounter {
     increment(count: Count): void
     maybeAdvance(now: Instant, newCurrrentBucket: Count): Count | null
-    // query(index: Int, numBuckets: Int): Count
+    query(
+        index: Int,
+        numBuckets: Int,
+        reducer: (a: number, b: Count) => number
+    ): number
     checkInvariant(): OptionalErrorMessage
 }
 
 class IntervalData {
-    currentBucket: Count = 0
     runningTotal: Count = 0
-    previousBuckets: Array<Count> = []
+    buckets: Array<Count> = []
     lastTick: Instant = 0
 
     static empty(numberBuckets: Int, lastTick: Instant): IntervalData {
-        const data = new IntervalData()
-        data.previousBuckets = new Array(numberBuckets - 1).fill(0)
-        data.lastTick = lastTick
-        return data
+        return new IntervalData(lastTick, new Array(numberBuckets).fill(0))
+    }
+
+    constructor(lastTick: Instant, buckets: Array<Count>) {
+        this.lastTick = lastTick
+        this.buckets = buckets
+        this.runningTotal = buckets.reduce((a, b) => a + b)
     }
 
     increment(count: Count) {
-        this.currentBucket += count
+        this.buckets[0] += count
         this.runningTotal += count
     }
 
+    query(
+        index: Int = 0,
+        numBuckets: Int = 1,
+        reducer: (a: number, b: Count) => number = (a, b) => a + b
+    ): number {
+        if (index < 0) {
+            index = this.buckets.length + index
+        }
+        if (numBuckets < 0) {
+            index += numBuckets
+            numBuckets *= -1
+        }
+        return this.buckets.slice(index, index + numBuckets).reduce(reducer)
+    }
+
     rotate(numRotations: Int, newCurrent: Count): Count {
-        let bucket = this.currentBucket
         let overflow = 0
         for (let i = 0; i < numRotations; i++) {
-            overflow += this.previousBuckets.pop() ?? 0
-            this.previousBuckets.unshift(bucket)
-            bucket = 0
+            overflow += this.buckets.pop() ?? 0
+            this.buckets.unshift(0)
         }
 
         this.runningTotal += newCurrent - overflow
-        this.currentBucket = newCurrent
+        this.buckets[0] = newCurrent
 
         return overflow
     }
 
     checkInvariant(): OptionalErrorMessage {
-        const expected = this.previousBuckets.reduceRight(
-            (a, b) => a + b,
-            this.currentBucket
-        )
+        const expected = this.buckets.reduceRight((a, b) => a + b)
         const observed = this.runningTotal
 
         if (expected !== observed) {
@@ -72,7 +88,7 @@ export class SingleIntervalCounter implements IntervalCounter {
     private config: IntervalConfig
 
     public get current(): Count {
-        return this.data.currentBucket
+        return this.data.buckets[0]
     }
 
     public get total(): Count {
@@ -104,13 +120,21 @@ export class SingleIntervalCounter implements IntervalCounter {
         return this.data.rotate(numRollovers, newCurrrentBucket)
     }
 
+    query(
+        index: Int = 0,
+        numBuckets: Int = 1,
+        reducer: (a: number, b: Count) => number = (a, b) => a + b
+    ): number {
+        return this.data.query(index, numBuckets, reducer)
+    }
+
     private numRollovers(now: Instant): Int {
         return Math.floor((now - this.data.lastTick) / this.config.interval)
     }
 
     checkInvariant(): OptionalErrorMessage {
-        const observed = this.data.previousBuckets.length
-        const expected = this.config.numBuckets - 1
+        const observed = this.data.buckets.length
+        const expected = this.config.numBuckets
         if (observed !== expected) {
             return `Expected number of buckets to be ${expected}, but found ${observed}`
         }
